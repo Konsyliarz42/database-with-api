46d6889ae29c9eec3e2b957392597fc8f8d0a421
Tomasz Kordiak <dotomika@gmail.com>
2020-10-29 17:59:25 +0100

    Execute_sql_script can run all script

--- database.py
+++ database.py
@@ -29,57 +29,88 @@ def create_connection(db_file=False):
     return connection
 
 #--------------------------------
-def add_song(connection, song):
-    """Add song to table 'songs' in database.
-    connection  - connection with database
-    song        - tuple with information about song
-                  (song_id, band, album, nr, title)"""
-
-    script  = create['song']
-    scr     = script
-    son     = list(song)
-    son.reverse()
-    
-    while '?' in scr:
-        scr = scr.replace('?', str(son[scr.count('?') - 1]), 1)
-
-    if connection:
-        try:
-            cursor = connection.cursor()
-
-            print(f"Execute:{scr}\n")
-            cursor.execute(script, song)
-        except sys.exc_info()[0] as error:
-            print('Error:', error)
-        else:
-            connection.commit()
-            return cursor.lastrowid
-    else:
-        print("Error: Connection is lost")
-
-#--------------------------------
 def execute_sql_scripts(connection, scripts):
     """Execute the script in list of scripts.
     connection  - connection with database
-    scripts     - list of scripts from 'sql_sripts.py'"""
+    scripts     - script or list of scripts from 'sql_sripts.py'
+                  if script require arguments use a dictionary with
+                  'script' (type: string) and 'arguments' (type: tuple or string)
+    
+    Example 1:
+        execute_sql_scripts(connection=connection, scripts=create['table_songs'])
 
-    if type(scripts) == str:
+    Example 2:
+        execute_sql_scripts(
+            connection  = connection,
+            scripts     = {'script': create['song'], 'arguments': (song_id, band, album, nr, title)}
+        )
+
+    Example 3:
+        execute_sql_scripts(
+            connection  = connection,
+            scripts     = [
+                create['table_songs'],
+                {'script': create['song'], 'arguments': (song_id, band, album, nr, title)}
+            ]
+        )"""
+
+    # Convert scripts to list
+    if type(scripts) in [dict, str]:
         scripts = [scripts]
-    else:
-        print("Error: Argument 'scripts' cannot be converted to list")
-        return None
 
+    elif type(scripts) != list:
+        print(f"Error: Script cannot be converted to list (must be list, set or string but not {type(scripts).__name__})")
+        return None
+    
+    # Check scripts arguments
+    for script in scripts:
+        if type(script) == dict:
+            ind     = scripts.index(script)
+            scr     = script['script']
+            arg     = script['arguments']
+
+            if type(arg) not in [tuple, str]:
+                print(f"Error: Arguments for script nr {ind} must be list or string not {type(arg).__name__}")
+                return None
+            
+            if scr.count('?') > len(arg):
+                print(f"Error: Arguments for script nr {ind} is not enough")
+                return None
+            elif scr.count('?') < len(arg):
+                print(f"Warring: Script nr {ind} not require arguments")
+                scripts[ind] = scr
+
+    # Execute script
     if connection:
+        result = list()
+
         for script in scripts:
+            arguments = False
+
+            if type(script) == dict:
+                arguments   = script['arguments']
+                script      = script['script']
+
             try:
                 cursor = connection.cursor()
 
-                print(f"Execute:{script}\n")
-                cursor.execute(script)
+                if arguments:
+                    print(f"Execute with arguments {arguments}:\n{script}\n")
+                    cursor.execute(script, arguments)
+                    connection.commit()
+                else:
+                    print(f'Execute: {script}\n')
+                    cursor.execute(script)
             except sys.exc_info()[0] as error:
                 print('Error:', error)
+            else:
+                if 'SELECT' in script:
+                    result.append(cursor.fetchall())
+
+        return result
     else:
         print("Error: Connection is lost")
+        return None
 
 #================================================================
 if __name__ == '__main__':
@@ -87,8 +118,16 @@ if __name__ == '__main__':
     connection      = create_connection(database_file)
 
     if connection:
-        execute_sql_scripts(connection, create['table_songs'])
-        add_song(connection, ('uuid', 'Nachtblut', 'Vanitas', 4, 'Das Puppenhaus'))
+
+        result = execute_sql_scripts(
+            connection  = connection,
+            scripts     = [
+                create['table_songs'],
+                {'script': create['song'], 'arguments': (325, 'Band', 'Album', 0, 'Title')}
+            ]
+        )
+
+        print('result =', result)
 
         #----------------
         connection.close()
